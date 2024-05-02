eb52b343c48279acad6233f7955b95835cd9a13b
Aloento <11802769+Aloento@users.noreply.github.com>
2024-05-03 01:14:16 +0200

    chore: Update npm dependencies and remove unused files

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "antd";
+import React from "react";
+
+function RootLayout({ children }: React.PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>
+        <AntdRegistry>
+          <ConfigProvider theme={{ cssVar: true, hashed: false }}>
+            {children}
+          </ConfigProvider>
+        </AntdRegistry>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
