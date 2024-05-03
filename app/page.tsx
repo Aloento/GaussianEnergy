4c84b2bc25b31b195d1a4976bc6489fbfa7863ad
Aloento <11802769+Aloento@users.noreply.github.com>
2024-05-03 17:21:11 +0200

    chore: Refactor Home page to use Parser component for file upload functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import { InboxOutlined } from "@ant-design/icons";
 import { Divider } from "antd";
-import Dragger from "antd/es/upload/Dragger";
 import { Header } from "./Header";
+import { Parser } from "./Parser";
 
 function Home() {
   return (
@@ -10,16 +9,7 @@ function Home() {
 
       <Divider className="!m-0" />
 
-      <div className="flex">
-        <Dragger action="/api" accept=".log" maxCount={1} className="h-fit">
-          <p className="ant-upload-drag-icon">
-            <InboxOutlined />
-          </p>
-          <p className="ant-upload-text">
-            Click or drag log file to this area to upload
-          </p>
-        </Dragger>
-      </div>
+      <Parser />
     </div>
   );
 }
